ccb3498e5757e8c68c4ffe0a87a8122704fa7922
andrewwongdgnt <andrew.wong.dgnt@gmail.com>
2014-05-19 13:09:45 -0700

    new file structure, new video, better music loop, better ad display

---
+++ King of Wings/Assets/Plugins/AdMobPlugin/AdMobPluginDemoScript.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+using System;
+
+// Example script showing how you can easily call into the AdMobPlugin.
+public class AdMobPluginDemoScript : MonoBehaviour {
+
+	public  static Boolean allowAds=true;
+    void Start()
+    {
+		try{
+			if (allowAds){
+				print("Started");
+				AdMobPlugin.CreateBannerView("ca-app-pub-5102038243868958/5889446821",
+	                                     AdMobPlugin.AdSize.Banner,
+	                                     false);
+		        print("Created Banner View");
+		        AdMobPlugin.RequestBannerAd(false);
+		        print("Requested Banner Ad");
+				allowAds=false;
+			}
+		} catch(Exception e){
+
+		}
+    }
+
+    void OnEnable()
+    {
+		print("Registering for AdMob Events");
+        AdMobPlugin.ReceivedAd += HandleReceivedAd;
+        AdMobPlugin.FailedToReceiveAd += HandleFailedToReceiveAd;
+        AdMobPlugin.ShowingOverlay += HandleShowingOverlay;
+        AdMobPlugin.DismissedOverlay += HandleDismissedOverlay;
+        AdMobPlugin.LeavingApplication += HandleLeavingApplication;
+    }
+
+    void OnDisable()
+    {
+        print("Unregistering for AdMob Events");
+		AdMobPlugin.ReceivedAd -= HandleReceivedAd;
+        AdMobPlugin.FailedToReceiveAd -= HandleFailedToReceiveAd;
+        AdMobPlugin.ShowingOverlay -= HandleShowingOverlay;
+        AdMobPlugin.DismissedOverlay -= HandleDismissedOverlay;
+        AdMobPlugin.LeavingApplication -= HandleLeavingApplication;
+    }
+
+    public void HandleReceivedAd()
+    {
+        print("HandleReceivedAd event received");
+    }
+
+    public void HandleFailedToReceiveAd(string message)
+    {
+        print("HandleFailedToReceiveAd event received with message:");
+        print(message);
+    }
+
+    public void HandleShowingOverlay()
+    {
+        print("HandleShowingOverlay event received");
+    }
+
+    public void HandleDismissedOverlay()
+    {
+        print("HandleDismissedOverlay event received");
+    }
+
+    public void HandleLeavingApplication()
+    {
+        print("HandleLeavingApplication event received");
+    }
+}
